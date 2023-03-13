b0eb309ba3a90243945864bbc3b20bc6bdef2531
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-03-13 13:31:27 +0000

    set up react, added pages folder with home, contact & project pages. Added Components folder and 2 components, gallery & Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Project from './pages/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+
+        <Routes>
+
+          <Route path="/" element={<Home/>} />
+          <Route path="Contact" element={<Contact/>} />
+          <Route path="Project" element={<Project/>} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project =() => {
+    return <div>
+        <header>
+        Project
+        </header>
+        
+    </div>
+}
+
+export default Project
